3da29af1a78857da0238339033e65c8e8e1bd703
Naman Arora <namanarora166@gmail.com>
2025-03-27 10:06:02 -0500

    refactored router to use methods

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -14,36 +14,51 @@ type AuthRouter = {
   };
 };
 
-export const router = new Hono<AuthRouter>({ strict: true })
-  .use(trimTrailingSlash())
-  .use("*", async (c, next) => {
-    const session = await auth.api.getSession({ headers: c.req.raw.headers });
+export const router = new Hono<AuthRouter>({ strict: true });
 
-    if (!session) {
-      c.set("user", null);
-      c.set("session", null);
-      return next();
-    }
+router.use(trimTrailingSlash());
 
-    c.set("user", session.user);
-    c.set("session", session.session);
+router.use("*", async (c, next) => {
+  const session = await auth.api.getSession({ headers: c.req.raw.headers });
+
+  if (!session) {
+    c.set("user", null);
+    c.set("session", null);
     return next();
-  })
-  .get("/", (c) => c.text("Hello World"))
-  .on(["POST", "GET"], "/auth/*", (c) => {
-    return auth.handler(c.req.raw);
-  })
-  .get("/session", async (c) => {
-    const session = c.get("session");
-    const user = c.get("user");
+  }
 
-    if (!user) return c.body(null, 401);
+  c.set("user", session.user);
+  c.set("session", session.session);
+  return next();
+});
 
-    return c.json({
-      session,
-      user,
-    });
+router.get("/", (c) => c.text("Hello World"));
+
+router.on(["POST", "GET"], "/auth/*", (c) => {
+  return auth.handler(c.req.raw);
+});
+
+router.get("/session", async (c) => {
+  const session = c.get("session");
+  const user = c.get("user");
+
+  if (!user) return c.body(null, 401);
+
+  return c.json({
+    session,
+    user,
   });
+});
+
+router.get("/test", (c) => {
+  const user = c.get("user");
+  const session = c.get("session");
+
+  if (!user) return c.text("Unauthorized", 401);
+  if (!session) return c.text("Session expired", 401);
+
+  return c.text(`Hi, ${user.name}!`);
+});
 
 export const api = new Hono().route("/api", router);
 
