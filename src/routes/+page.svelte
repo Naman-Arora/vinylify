a66fe5f666345d82ab701cb3d388ef666283f0f5
Naman Arora <namanarora166@gmail.com>
2025-03-27 00:24:55 -0500

    added title to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,10 @@
   const session = authClient.useSession();
 </script>
 
+<svelte:head>
+  <title>Records</title>
+</svelte:head>
+
 <div class="flex flex-col gap-4 p-10">
   <h1 class="text-4xl font-bold">Welcome to Records!</h1>
 
