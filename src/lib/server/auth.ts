89f8fdf744be911f6a4e4c940ed4505404de6f97
Naman Arora <namanarora166@gmail.com>
2025-03-27 22:44:41 -0500

    updated spotify auth

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,38 +1,45 @@
+import { db } from "./db";
 import { env } from "$env/dynamic/private";
 import { PUBLIC_BASE_URL } from "$env/static/public";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
-import { db } from "./db";
 
 export const auth = betterAuth({
-	baseURL: PUBLIC_BASE_URL,
-	database: drizzleAdapter(db, {
-		provider: "pg",
-	}),
+  baseURL: PUBLIC_BASE_URL,
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
 
-	// https://www.better-auth.com/docs/concepts/session-management#session-caching
-	session: {
-		cookieCache: {
-			enabled: true,
-			maxAge: 5 * 60, // 5 minutes
-		},
-	},
+  // https://www.better-auth.com/docs/concepts/session-management#session-caching
+  session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 5 * 60, // 5 minutes
+    },
+  },
 
-	// https://www.better-auth.com/docs/concepts/oauth
-	socialProviders: {
-		github: {
-			clientId: env.GITHUB_CLIENT_ID!,
-			clientSecret: env.GITHUB_CLIENT_SECRET!,
-		},
-		spotify: {
-			clientId: env.SPOTIFY_CLIENT_ID!,
-			clientSecret: env.SPOTIFY_CLIENT_SECRET!,
-		},
-	},
-
-	// https://www.better-auth.com/docs/authentication/email-password
-	// emailAndPassword: {
-	//   enabled: true,
-	// },
+  // https://www.better-auth.com/docs/concepts/oauth
+  socialProviders: {
+    github: {
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
+    },
+    spotify: {
+      clientId: env.SPOTIFY_CLIENT_ID,
+      clientSecret: env.SPOTIFY_CLIENT_SECRET,
+      disableDefaultScope: true,
+      scope: [
+        "user-read-email",
+        "user-read-private",
+        // "user-read-playback-state",
+        // "user-modify-playback-state",
+        "user-library-read",
+        // "user-library-modify",
+        "user-top-read",
+        "user-follow-read",
+        // "user-follow-modify",
+      ],
+    },
+  },
 });
