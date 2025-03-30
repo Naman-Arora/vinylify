488063a586c867264b7988272b9ec27e3450d169
Naman Arora <namanarora166@gmail.com>
2025-03-30 12:48:36 -0500

    added table to handle shares

---
+++ src/lib/server/db/schema/share.schema.ts
@@ -0,0 +1,27 @@
+import { user } from "./auth.schema";
+import { jsonb, pgTable, text, timestamp, uniqueIndex } from "drizzle-orm/pg-core";
+import { customAlphabet } from "nanoid";
+
+const nanoid = customAlphabet("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", 7);
+
+export const share = pgTable(
+  "share",
+  {
+    id: text("id")
+      .unique()
+      .notNull()
+      .$defaultFn(() => nanoid()),
+    userId: text("user_id")
+      .primaryKey()
+      .unique()
+      .notNull()
+      .references(() => user.id, { onDelete: "cascade" }),
+    trackIds: jsonb("track_ids").$type<string[]>().notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at")
+      .defaultNow()
+      .notNull()
+      .$onUpdate(() => new Date()),
+  },
+  (table) => [uniqueIndex("id_index").on(table.id)],
+);
