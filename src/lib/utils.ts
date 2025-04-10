612bbe6007066abe24c466a57654192079bca29d
Naman Arora <namanarora166@gmail.com>
2025-04-10 13:19:09 -0500

    removed 3d utils and added them to lib/utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,21 @@
-import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from "clsx";
+import { MathUtils, type EulerOrder } from "three";
+
+export type Position = [x: number, y: number, z: number];
+export type Rotation = [x: number, y: number, z: number, order: EulerOrder];
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function trimString(str: string, maxLength: number): string {
+  return str.length > maxLength ? str.slice(0, maxLength) + "…" : str;
+}
+
+export function degreesToEuler(xDeg: number, yDeg: number, zDeg: number): Rotation {
+  const xRad = MathUtils.degToRad(xDeg);
+  const yRad = MathUtils.degToRad(yDeg);
+  const zRad = MathUtils.degToRad(zDeg);
+  return [xRad, yRad, zRad, "XYZ"];
 }
