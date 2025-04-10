b7bcf76cadeea947cb46e39c6772e8bf12ad4118
Naman Arora <namanarora166@gmail.com>
2025-04-10 13:20:20 -0500

    removed uneccessary console logs

--- src/routes/share/[id]/+page.server.ts
+++ src/routes/share/[id]/+page.server.ts
@@ -6,7 +6,6 @@ export const load: PageServerLoad = async ({ fetch, params }) => {
   const api = makeAPIClient(fetch);
   const response = await api.share.$get({ query: { id: params.id } });
   const data = await response.json();
-  // console.log(data);
 
   if (!response.ok || "error" in data) {
     return error(404, { message: "Link does not exist" });
