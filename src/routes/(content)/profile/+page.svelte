b96d38028ee3e83826aa9d07c1051038d0d22a71
Naman Arora <namanarora166@gmail.com>
2025-04-14 13:45:22 -0500

    added signout button to profile page

--- src/routes/(content)/profile/+page.svelte
+++ src/routes/(content)/profile/+page.svelte
@@ -28,26 +28,8 @@
   <!-- <article class="prose prose-xl w-[65ch]">
     <h1>Profile</h1>
 
-    <section class="not-prose">
-      <div role="alert" class="alert alert-info alert-soft">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="lucide lucide-info-icon lucide-info"
-          ><circle cx="12" cy="12" r="10" /><path d="M12 16v-4" /><path d="M12 8h.01" /></svg
-        >
-        <span>12 unread messages. Tap to see.</span>
-      </div>
-    </section>
-  </article> -->
-  <div class="flex flex-row items-center gap-4">
+<main class="flex min-h-screen flex-col items-center justify-start gap-8 py-8">
+  <section class="flex flex-row items-center gap-4">
     <div class={["avatar avatar-placeholder"]}>
       <div class="bg-neutral text-neutral-content w-24 rounded-full">
         {#if $session.data?.user.image}
@@ -59,9 +41,52 @@
         {/if}
       </div>
     </div>
-    <div class="">
+    <div class="flex flex-col gap-2">
       <p class="text-xl font-bold">{name}</p>
       <p class="text-base font-semibold">{email}</p>
+      <button
+        onclick={async () => await authClient.signOut()}
+        class="btn btn-sm btn-soft btn-error"
+      >
+        Sign Out
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide-log-out size-4 fill-none stroke-current stroke-2"
+          ><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" /><polyline
+            points="16 17 21 12 16 7"
+          /><line x1="21" x2="9" y1="12" y2="12" /></svg
+        >
+      </button>
     </div>
-  </div>
+  </section>
+  <!-- <section>
+    <div role="alert" class="alert alert-info alert-soft">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="lucide lucide-info-icon lucide-info"
+        ><circle cx="12" cy="12" r="10" /><path d="M12 16v-4" /><path d="M12 8h.01" /></svg
+      >
+      <span>12 unread messages. Tap to see.</span>
+    </div>
+    <button
+      class="btn btn-error"
+      onclick={() => {
+        modalType = "deleteAccount";
+        modal?.show();
+      }}>Delete Account</button
+    > 
+    </section>-->
 </main>
