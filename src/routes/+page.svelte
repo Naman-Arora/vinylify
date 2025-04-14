5b3c1a9fadafd02f0e693fd69eccd020c8dc3761
Naman Arora <namanarora166@gmail.com>
2025-04-14 13:38:31 -0500

    accidentally removed sign in button, fixed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -52,29 +52,6 @@
         >
       </a>
     </div>
-    <!-- <button
-      onclick={async () => {
-        await authClient.signOut();
-      }}
-      class="btn btn-ghost btn-secondary"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="lucide-log-out size-6"
-        ><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" /><polyline
-          points="16 17 21 12 16 7"
-        /><line x1="21" x2="9" y1="12" y2="12" /></svg
-      >
-      Sign Out
-    </button>
   {:else}
     <button
       class="flex w-72 flex-row items-center gap-4 rounded-full bg-green-500 px-6 py-3 font-bold text-white transition duration-300 hover:bg-green-600"
@@ -96,7 +73,7 @@
         /></svg
       >
       Sign in with Spotify
-    </button> -->
+    </button>
   {/if}
 </div>
 <Footer />
