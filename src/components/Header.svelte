e75902ddd88a9df4000c8bc55e9484b07fed85f7
Naman Arora <namanarora166@gmail.com>
2025-04-13 22:38:34 -0500

    refactored header and footer to seperate files and fixed darkmode implementation

---
+++ src/components/Header.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { authClient } from "$lib/auth-client";
+  const session = authClient.useSession();
+</script>
+
+<header class="navbar fixed top-0 left-0 bg-base-200/75 shadow-sm backdrop-blur-md">
+  <div class="flex-1">
+    <a href="/" aria-label="home" class="btn btn-ghost text-xl font-bold">Vinylify</a>
+  </div>
+  {#if $session.data}
+    <a
+      href="/profile"
+      aria-label="Profile"
+      class={["btn btn-ghost btn-circle avatar", !$session.data.user.image && "avatar-placeholder"]}
+    >
+      <div class="bg-neutral text-neutral-content w-10 rounded-full">
+        {#if $session.data.user.image}
+          <img alt="Profile" src={$session.data.user.image} />
+        {:else}
+          <span class="text-xl">{$session.data.user.name.at(0)?.toLocaleUpperCase()}</span>
+        {/if}
+      </div>
+    </a>
+  {/if}
+</header>
