5078b776a312bf9e5e2675a4c5c215ebb941c612
Naman Arora <namanarora166@gmail.com>
2025-04-15 13:11:56 -0500

    updated readme and about to fully explain website

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# Vinylify
+# About Vinylify
+
+**[Vinylify](https://vinylify.naman.zip/)** is a music service that allows you and your friends to discover and share your top songs through vinyl records.
+
+To get started, visit the [website](https://vinylify.naman.zip/) and login with your Spotify account. Then, visit the [view](https://vinylify.naman.zip/view) page to see your top songs. Add your top songs into a playlist by clicking the _add playlist_ button, and share your top songs with your friends by clicking the _share_ button. Copy your unique share link and send it to your friends to showcase your tops songs!
+
+For example, click [here](https://vinylify.naman.zip/share/RwNohef) to see my top songs!
+
+---
+
+Vinylify is built and maintained by [Naman Arora](https://namanarora.net/), and was my submission for [Record Hacks](https://recordhacks.devpost.com/project-gallery), where the project won best design.
+
+Vinylify is fully open-source and the source code can be found on [GitHub](https://github.com/Naman-Arora/vinylify).
+
+If you find any bugs, or have any issues and/or suggestions, please make sure to either open an issue on GitHub or contact me at [vinylify@namanarora.net](mailto:vinylify@namanarora.net).
+
+## Tech Stack
+
+Vinylify is built using the following technologies:
+
+- [SvelteKit](https://kit.svelte.dev/), as the Javascript framework
+- [Threlte](https://threlte.xyz/) and [Three.js](https://threejs.org/), for 3D graphics
+- [Tailwind CSS](https://tailwindcss.com/) and [DaisyUI](https://daisyui.com/), for styling
+- [Hono](https://hono.dev/), for the api and api client
+- [Spotify Developer API](https://developer.spotify.com/), for user music data
+- [Better-Auth](https://better-auth.com/), for authentication and authorization
+- [Drizzle ORM](https://orm.drizzle.team/), for database management
+- [PostgreSQL](https://www.postgresql.org/), hosted on [Neon](https://neon.tech/), as the database
+- [Vercel](https://vercel.com/), for deployment
+- [GitHub](https://github.com/), for continuous integration and deployment
+
+## Credits
+
+- The vinyl icon used as the favicon for this website is sourced from [smalllikeart on Flaticon](https://www.flaticon.com/free-icons/vinyl).
--- src/routes/(content)/about/+page.svelte
+++ src/routes/(content)/about/+page.svelte
@@ -4,20 +4,73 @@
 
 <main class="flex justify-center px-4 py-8 md:px-8">
   <article class="prose lg:prose-lg xl:prose-xl">
-    <h1>About Vinylify</h1>
+    <h1 id="about-vinylify">About Vinylify</h1>
     <p>
-      Vinylify is a music streaming service that allows users and their friends to discover their
-      favorite vinyl records.
+      <strong><a href="https://vinylify.naman.zip/">Vinylify</a></strong> is a music service that allows
+      you and your friends to discover and share your top songs through vinyl records.
     </p>
-    <h2>Our Mission</h2>
     <p>
-      Our mission is to provide a platform for vinyl enthusiasts to connect, share, and discover new
-      music.
+      To get started, visit the <a href="https://vinylify.naman.zip/">website</a> and login with
+      your Spotify account. Then, visit the <a href="https://vinylify.naman.zip/view">view</a> page
+      to see your top songs. Add your top songs into a playlist by clicking the
+      <em>add playlist</em>
+      button, and share your top songs with your friends by clicking the <em>share</em> button. Copy
+      your unique share link and send it to your friends to showcase your tops songs!
     </p>
-    <!-- <h2>Our Team</h2>
     <p>
-      Our team is made up of passionate vinyl enthusiasts who are dedicated to providing the best
-      possible experience for our users.
-    </p> -->
+      For example, click <a href="https://vinylify.naman.zip/share/RwNohef">here</a> to see my top songs!
+    </p>
+    <hr />
+    <p>
+      Vinylify is built and maintained by <a href="https://namanarora.net/">Naman Arora</a>, and was
+      my submission for <a href="https://recordhacks.devpost.com/project-gallery">Record Hacks</a>,
+      where the project won best design.
+    </p>
+    <p>
+      Vinylify is fully open-source and the source code can be found on <a
+        href="https://github.com/Naman-Arora/vinylify">GitHub</a
+      >.
+    </p>
+    <p>
+      If you find any bugs, or have any issues and/or suggestions, please make sure to either open
+      an issue on GitHub or contact me at <a href="mailto:vinylify@namanarora.net"
+        >vinylify@namanarora.net</a
+      >.
+    </p>
+    <h2 id="tech-stack">Tech Stack</h2>
+    <p>Vinylify is built using the following technologies:</p>
+    <ul>
+      <li><a href="https://kit.svelte.dev/">SvelteKit</a>, as the Javascript framework</li>
+      <li>
+        <a href="https://threlte.xyz/">Threlte</a> and <a href="https://threejs.org/">Three.js</a>,
+        for 3D graphics
+      </li>
+      <li>
+        <a href="https://tailwindcss.com/">Tailwind CSS</a> and
+        <a href="https://daisyui.com/">DaisyUI</a>, for styling
+      </li>
+      <li><a href="https://hono.dev/">Hono</a>, for the api and api client</li>
+      <li>
+        <a href="https://developer.spotify.com/">Spotify Developer API</a>, for user music data
+      </li>
+      <li>
+        <a href="https://better-auth.com/">Better-Auth</a>, for authentication and authorization
+      </li>
+      <li><a href="https://orm.drizzle.team/">Drizzle ORM</a>, for database management</li>
+      <li>
+        <a href="https://www.postgresql.org/">PostgreSQL</a>, hosted on
+        <a href="https://neon.tech/">Neon</a>, as the database
+      </li>
+      <li><a href="https://vercel.com/">Vercel</a>, for deployment</li>
+      <li><a href="https://github.com/">GitHub</a>, for continuous integration and deployment</li>
+    </ul>
+    <h2 id="credits">Credits</h2>
+    <ul>
+      <li>
+        The vinyl icon used as the favicon for this website is sourced from <a
+          href="https://www.flaticon.com/free-icons/vinyl">smalllikeart on Flaticon</a
+        >.
+      </li>
+    </ul>
   </article>
 </main>
