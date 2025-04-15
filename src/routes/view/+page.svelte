9d338adaa4a366f75fdc350ab558dcdbae4261a3
Naman Arora <namanarora166@gmail.com>
2025-04-15 15:41:56 -0500

    refactor sharelink into seperate component

--- src/routes/view/+page.svelte
+++ src/routes/view/+page.svelte
@@ -2,6 +2,7 @@
   import { makeAPIClient } from "$lib/api/client";
   import { Vinylify } from "$lib/vinylify.svelte";
   import Scene from "@components/3d/Scene.svelte";
+  import ShareLink from "@components/ShareLink.svelte";
   import SongInfo from "@components/SongInfo.svelte";
   import { Canvas } from "@threlte/core";
   import { toast } from "svelte-sonner";
@@ -76,57 +77,7 @@
     <h3 class="text-2xl font-bold">{dialogContent.title}</h3>
     <p>{dialogContent.message}</p>
     {#if dialogContent.link}
-      <div
-        class={[
-          "flex w-full flex-col items-center justify-between overflow-hidden rounded bg-gray-200 p-2 font-mono text-sm text-black sm:flex-row",
-        ]}
-      >
-        {dialogContent.link}
-        <div class="flex flex-row gap-0.5">
-          <button
-            aria-label="Copy"
-            onclick={() => navigator.clipboard.writeText(dialogContent.link)}
-            class="btn btn-ghost btn-square"
-            ><svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="lucide lucide-clipboard-copy-icon lucide-clipboard-copy"
-              ><rect width="8" height="4" x="8" y="2" rx="1" ry="1" /><path
-                d="M8 4H6a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-2"
-              /><path d="M16 4h2a2 2 0 0 1 2 2v4" /><path d="M21 14H11" /><path
-                d="m15 10-4 4 4 4"
-              /></svg
-            ></button
-          >
-          <button
-            aria-label="Open in new tab"
-            onclick={() => window.open(dialogContent.link, "_blank")}
-            class="btn btn-ghost btn-square"
-            ><svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="lucide-external-link"
-              ><path d="M15 3h6v6" /><path d="M10 14 21 3" /><path
-                d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"
-              /></svg
-            >
-          </button>
-        </div>
-      </div>
+      <ShareLink url={dialogContent.link} />
     {/if}
   </div>
   <form method="dialog" class="modal-backdrop">
